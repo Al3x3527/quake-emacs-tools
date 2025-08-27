b3b6ea8f2ef03ed52a8b3cead0eea7daf78d096c
Al3x3527 <157188190+Al3x3527@users.noreply.github.com>
2025-08-27 15:49:01 -0500

    Create quake-maker.el

---
+++ quake-maker/quake-maker.el
@@ -0,0 +1,11 @@
+;;; lisp/quake-maker/quake-maker.el -*- lexical-binding: t; -*-
+
+(defun quake-make()
+"update progs.dat files."
+  (interactive)
+  (copy-file "~/GitHub/Project-E/qwprogs.dat" "~/Quake/FTEQW/enoch/" t)
+  (with-current-buffer(get-buffer-create "qm-out")
+   (eshell-command "cd ~/Quake/FTEQW/ && ./fteqw64 > #<buffer qm-out>")
+   (pop-to-buffer "qm-out"'((display-buffer-at-bottom)
+                             (inhibit-same-window . t)))
+  ))
