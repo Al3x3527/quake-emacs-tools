f30b8e364638183d8a66168dc59b47174dccb3ed
Al3x3527 <157188190+Al3x3527@users.noreply.github.com>
2025-08-28 13:21:23 -0500

    help window

--- quake-maker/quake-maker.el
+++ quake-maker/quake-maker.el
@@ -4,8 +4,10 @@
 "update progs.dat files."
   (interactive)
   (copy-file "~/GitHub/Project-E/qwprogs.dat" "~/Quake/FTEQW/enoch/" t)
+  (with-help-window "qm-out"
   (with-current-buffer(get-buffer-create "qm-out")
    (eshell-command "cd ~/Quake/FTEQW/ && ./fteqw64 > #<buffer qm-out>")
    (pop-to-buffer "qm-out"'((display-buffer-at-bottom)
-                             (inhibit-same-window . t)))
-  ))
+                             (inhibit-same-window . t)
+   ))
+  )))
