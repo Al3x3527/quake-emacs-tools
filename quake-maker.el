2a3fbcc60ffb232cb5edaaabdd4a2b4a14d196d8
Al3x3527 <157188190+Al3x3527@users.noreply.github.com>
2026-05-01 14:08:44 -0500

    re-write

--- quake-maker.el
+++ quake-maker.el
@@ -1,22 +1,43 @@
-;;; lisp/quake-maker/quake-maker.el -*- lexical-binding: t; -*-
+;;; quake-maker.el -*- lexical-binding: t; -*-
 
-(defun quake-make()
-"update progs.dat files."
-(interactive)
-;(setq gamedirpath '("~/Programs/project_enoch/FTEQW/enoch"))
-  (setq progslist '(
-                    "~/GitHub/Project-E/qwprogs.dat"
-                    "~/GitHub/Project-E/menu.dat"
-                    "~/GitHub/Project-E/csprogs.dat"))
-  (while progslist
-      (copy-file (car progslist) "~/Programs/project_enoch/FTEQW/enoch/" t)
-      (setq progslist (cdr progslist)))
-  (with-help-window "qm-out"
-  (with-current-buffer(get-buffer-create "qm-out")
-    (print "progs copied.")
-   (eshell-command "cd ~/Programs/project_enoch/FTEQW/ && ./fteqw64 +map test_biped > #<buffer qm-out>")
-   (pop-to-buffer "qm-out"'((display-buffer-at-bottom)
-                             (inhibit-same-window . t)
-   ))
-  )))
-;; if this doesn't work, try opening an eshell buffer manually, then close it.
+;;TODO make sure we can load quake config
+(load-file "/home/alex/GitHub/Project-E/quake.el")
+(require 'quake)
+
+(defun quake-copy-progs()
+  "update progs.dat files."
+  (interactive)
+
+;;; copy mod files
+  (let* (target-dir (expand-file-name quake-moddirpath quake-gamedirpath)))
+  (dolist (file quake-progslist)
+    (let ((src (expand-file-name file quake-project))
+          (dst (expand-file-name file target-dir))
+          (copy-file src dst t)
+          (with-current-buffer "qm-out"
+            (insert file " copied."))))))
+(provide 'quake-copy-progs)
+
+;;; run game
+(defun quake-run-engine ()
+  "run Quake engine from cmd line."
+  (interactive)
+    (let ((default-directory quake-gamedirpath))
+      (eshell-command
+       (concat "./" quake-engine " " quake-launch-args)
+       "qm-out")))
+; FIXME: eshell output inserted at point in current buffer
+(provide 'quake-run-engine)
+
+;;; test game
+(defun quake-test ()
+  "update progs and run Quake engine."
+  (interactive)
+  (with-current-buffer "qm-out"
+    (erase-buffer))
+  (quake-copy-progs)
+  (quake-run-engine)
+  (pop-to-buffer "qm-out" '((display-buffer-at-bottom))))
+(provide 'quake-test)
+
+;;; quake-maker.el ends here
